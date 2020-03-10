0d8cabfb3532d416e58dddb6824725078191c68f
Vincent Velthuizen <v.r.velthuizen@pl.hanze.nl>
2020-03-10 16:45:09 +0100

    exercise 7 (3)

--- third_set/exercise_7.py
+++ third_set/exercise_7.py
@@ -1,10 +1,12 @@
-n = 8
-threshold = 1e-3
+n = 25
+threshold = 1e-8
 approximation = n / 2
 
 while True:
     better = (approximation + n / approximation) / 2
     if abs(approximation - better) < threshold:
-        print(better)
         break
+    print("better\t", better)
     approximation = better
+
+print("best\t", better)
